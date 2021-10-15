27f85e5b6ec07d0003c6d9544a6bd4192dbea9be
山岡智裕 <>
2021-10-15 18:44:29 +0900

    add:コード調整ツール

--- react-ts-app/src/App.tsx
+++ react-ts-app/src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Basic1 from "./components/Basic1"
 
 function App() {
   return (
     <div className="App">
+      <Basic1 />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
